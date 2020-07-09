61905c3fb45140c44b58b3f72f078b3eec1065c1
João Guilherme <35269074+JoaoGomms@users.noreply.github.com>
2020-07-08 23:32:54 -0300

    Formulario de cadastro criado
    Conexão com o banco de dados bem sucedida
    
    Cadastro de Artigos no banco de dados finalizada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,50 @@
 const { Router } = require('express');
 const router = Router();
+const admin = require("firebase-admin");
+
+let serviceAccount = require('../../firebasesdk.json');
+
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
 
 router.get('/', (req, res) => {
 
-   res.render('index');
+    db.collection('Artigos').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                console.log(doc.id, '=>', doc.data());
+                res.render('index', {articles: doc.data()});
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        });
 
-})
+// res.render('index');
+
+
+});
+
+router.post('/new-article', (req, res) => {
+
+   console.log(req.body);
+   const newArticle = {
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      conteudo: req.body.content,
+      url: req.body.name,
+
+   };
+   db.collection('Artigos').add(newArticle)
+
+
+
+   res.send('recebido');
+
+});
 
 module.exports = router;

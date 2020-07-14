80577151cefc19e8d9ac2b4d3f1b6f84399b05bb
João Guilherme <35269074+JoaoGomms@users.noreply.github.com>
2020-07-13 21:47:00 -0300

    Adicionadas informações sobre os artigos já cadastrados

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,49 +1,49 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const router = Router();
 const admin = require("firebase-admin");
+
 
 let serviceAccount = require('../../firebasesdk.json');
 
 admin.initializeApp({
-   credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 let db = admin.firestore();
 
 
-router.get('/', (req, res) => {
+router.get('/',   async(req, res) => {
+    const articlesSnapshot = await db.collection('Artigos').get();
 
-    db.collection('Artigos').get()
-        .then((snapshot) => {
-            snapshot.forEach((doc) => {
-                console.log(doc.id, '=>', doc.data());
-                res.render('index', {articles: doc.data()});
-            });
-        })
-        .catch((err) => {
-            console.log('Error getting documents', err);
+    const articles = [];
+
+    articlesSnapshot.forEach((doc) => {
+        articles.push({
+            id: doc.id,
+            data: doc.data()
+
         });
 
-// res.render('index');
+    });
 
-
+    res.render('index', {articles: articles})
 });
+
 
 router.post('/new-article', (req, res) => {
 
-   console.log(req.body);
-   const newArticle = {
-      title: req.body.title,
-      subtitle: req.body.subtitle,
-      conteudo: req.body.content,
-      url: req.body.name,
+    console.log(req.body);
+    const newArticle = {
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        conteudo: req.body.content,
+        url: req.body.name,
 
-   };
-   db.collection('Artigos').add(newArticle)
+    };
+    db.collection('Artigos').add(newArticle)
 
 
-
-   res.send('recebido');
+    res.send('recebido');
 
 });
 
